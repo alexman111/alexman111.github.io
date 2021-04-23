95d5d472483fa915f5563a3cffbf4b1687dbb4e4
Alexander Krivosheev <alex19042001@tut.by>
2021-04-23 15:13:53 +0300

    quizes features

--- js/quizes.js
+++ js/quizes.js
@@ -7,14 +7,18 @@ window.onload = () => {
     for (i = 0; i < quests.length; i++) {
         let quest = quests[i]
         const newQuiz = document.createElement("div");
-
+        newQuiz.value = i;
         newQuiz.innerHTML = `   
             <div class="quiz__description">             
                 <p>${quest.questDescription}</p>
             </div>
         `
         mainContent.appendChild(newQuiz)
+        newQuiz.onclick = () => {
+            window.location = '/quiz.html?questNumber=' + newQuiz.value;
+        }
     }
+
 
 
 }
